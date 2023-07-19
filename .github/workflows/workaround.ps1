58d1814874d20d307c33e93d1bdb2b85ea858a03
Jon Carrier <jjcarrier@gmail.com>
2023-07-19 08:40:55 -0400

    Added CI workflows

---
+++ .github/workflows/workaround.ps1
@@ -0,0 +1,22 @@
+function Build-RequiredModuleFiles {
+    $data = Import-PowerShellDataFile .\*.psd1
+    [array]$requiredModules = $data.RequiredModules
+
+    if ($requiredModules) {
+        Push-Location
+        if ($IsWindows) {
+            Set-Location "$env:USERPROFILE/Documents/PowerShell/Modules"
+        } else {
+            Set-Location '~/.local/share/powershell/Modules'
+        }
+
+        foreach ($module in $requiredModules) {
+            $moduleName = $module.ModuleName
+            New-Item $moduleName -type Directory
+            Write-Output "Creating empty .psd1 file for module $modulename at $((Get-Location).Path)\$moduleName\$moduleName.psd1"
+            New-Item ".\$moduleName\$moduleName.psd1"
+        }
+
+        Pop-Location
+    }
+}
