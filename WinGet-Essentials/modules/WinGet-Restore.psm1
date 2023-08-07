e98f13d24ca6abe65bf0b481514bc670e8b15707
Jon Carrier <jjcarrier@gmail.com>
2023-08-07 16:52:25 -0400

    Added auto-migration of winget.packages.json
    New installations of this module will now check for the existence of
    winget.packages.json in other versions of the module installed on the
    system and automatically configure itself to use the latest one
    available (according to the module version, last modification time is
    not considered).

--- WinGet-Essentials/modules/WinGet-Restore.psm1
+++ WinGet-Essentials/modules/WinGet-Restore.psm1
@@ -106,8 +106,10 @@ function Restore-WinGetSoftware
         $ConfirmPreference = 'None'
     }
 
+    Initialize-WinGetRestore | Out-Null
     if (-not(Test-Path $PackageDatabase)) {
         Write-Error "`"$PackageDatabase`" does not exist. Please create this file and populate it with tagged winget package identifiers."
+        Write-Error "Use Initialize-WinGetRestore to setup a symlink to this file."
     }
 
     $MatchAnyScriptBlock = {
